7f988239879b4ae825782cacbeab7244bebab994
Rick Drizin <Drizin@users.noreply.github.com>
2020-08-12 21:31:40 -0400

    Wrapper around IDbConnection and IDbCommand so that we can inspect how Dapper is preparing our Commands

---
+++ src/DapperQueryBuilder.Tests/Helpers/UnitTestsDbCommand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DapperQueryBuilder.Tests
+{
+    /// <summary>
+    /// This is just a wrapper around IDbCommand, which allows us to inspect how Dapper is preparing our Commands
+    /// </summary>
+    public class UnitTestsDbCommand : IDbCommand
+    {
+        private readonly IDbCommand _cmd;
+        public UnitTestsDbCommand(IDbCommand command)
+        {
+            _cmd = command;
+        }
+
+        public string CommandText { get => _cmd.CommandText; set => _cmd.CommandText = value; }
+        public int CommandTimeout { get => _cmd.CommandTimeout; set => _cmd.CommandTimeout = value; }
+        public CommandType CommandType { get => _cmd.CommandType; set => _cmd.CommandType = value; }
+        public IDbConnection Connection { get => _cmd.Connection; set => _cmd.Connection = value; }
+
+        public IDataParameterCollection Parameters => _cmd.Parameters;
+
+        public IDbTransaction Transaction { get => _cmd.Transaction; set => _cmd.Transaction = value; }
+        public UpdateRowSource UpdatedRowSource { get => _cmd.UpdatedRowSource; set => _cmd.UpdatedRowSource = value; }
+
+        public void Cancel()
+        {
+            _cmd.Cancel();
+        }
+
+        public IDbDataParameter CreateParameter()
+        {
+            return _cmd.CreateParameter();
+        }
+
+        public void Dispose()
+        {
+            _cmd.Dispose();
+        }
+
+        public int ExecuteNonQuery()
+        {
+            return _cmd.ExecuteNonQuery();
+        }
+
+        public IDataReader ExecuteReader()
+        {
+            return _cmd.ExecuteReader();
+        }
+
+        public IDataReader ExecuteReader(CommandBehavior behavior)
+        {
+            return _cmd.ExecuteReader(behavior);
+        }
+
+        public object ExecuteScalar()
+        {
+            return _cmd.ExecuteScalar();
+        }
+
+        public void Prepare()
+        {
+            _cmd.Prepare();
+        }
+
+    }
+
+}
---
+++ src/DapperQueryBuilder.Tests/Helpers/UnitTestsDbConnection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DapperQueryBuilder.Tests
+{
+    /// <summary>
+    /// This is just a wrapper around IDbConnection, which allows us to inspect how Dapper is preparing our Commands
+    /// </summary>
+    public class UnitTestsDbConnection : IDbConnection
+    {
+        private readonly IDbConnection _conn;
+        public IDbCommand LastCommand { get; set; }
+
+        public UnitTestsDbConnection(IDbConnection connection)
+        {
+            _conn = connection;
+        }
+
+        public string ConnectionString { get => _conn.ConnectionString; set => _conn.ConnectionString = value; }
+
+        public int ConnectionTimeout => _conn.ConnectionTimeout;
+
+        public string Database => _conn.Database;
+
+        public ConnectionState State => _conn.State;
+
+        public IDbTransaction BeginTransaction()
+        {
+            return _conn.BeginTransaction();
+        }
+
+        public IDbTransaction BeginTransaction(IsolationLevel il)
+        {
+            return _conn.BeginTransaction(il);
+        }
+
+        public void ChangeDatabase(string databaseName)
+        {
+            _conn.ChangeDatabase(databaseName);
+        }
+
+        public void Close()
+        {
+            _conn.Close();
+        }
+
+        public IDbCommand CreateCommand()
+        {
+            LastCommand = new UnitTestsDbCommand(_conn.CreateCommand());
+            return LastCommand;
+        }
+
+        public void Dispose()
+        {
+            _conn.Dispose();
+        }
+
+        public void Open()
+        {
+            _conn.Open();
+        }
+    }
+
+}
