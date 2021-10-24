8de1c993ab62a1b031fbd8e1a943ce4ac30ec92c
Rick Drizin <Drizin@users.noreply.github.com>
2021-10-24 19:35:01 -0400

    build-release.cmd fix

--- src/build-release.cmd
+++ src/build-release.cmd
@@ -1,7 +1,7 @@
-rmdir /s /q "%HOMEPATH%\.nuget\packages\codegencs"
-rmdir /s /q "%HOMEPATH%\.nuget\packages\codegencs.schema"
+rmdir /s /q "%HOMEDRIVE%%HOMEPATH%\.nuget\packages\Dapper-QueryBuilder"
 
 if not exist packages-local mkdir packages-local
 
 dotnet build -c release DapperQueryBuilder\DapperQueryBuilder.csproj
 dotnet test  DapperQueryBuilder.Tests\DapperQueryBuilder.Tests.csproj
+dotnet pack -c release DapperQueryBuilder\DapperQueryBuilder.csproj
