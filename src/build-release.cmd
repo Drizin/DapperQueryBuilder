3e8d34dc79eea45dd124788ca5fc58891e7a77e7
Rick Drizin <Drizin@users.noreply.github.com>
2022-02-13 18:29:50 -0500

    - Deterministic Builds and Github SourceLink weren't working yet: Included symbol files (*.pdb) in the main .nupkg (AllowedOutputExtensionsInPackageBuildOutputFolder) - Version bump: 1.2.2 - Nupkg output folder moved outside of csproj

--- src/build-release.cmd
+++ src/build-release.cmd
@@ -2,6 +2,9 @@ rmdir /s /q "%HOMEDRIVE%%HOMEPATH%\.nuget\packages\Dapper-QueryBuilder"
 
 if not exist packages-local mkdir packages-local
 
+dotnet clean
 dotnet build -c release DapperQueryBuilder\DapperQueryBuilder.csproj
 dotnet test  DapperQueryBuilder.Tests\DapperQueryBuilder.Tests.csproj
-dotnet pack -c release DapperQueryBuilder\DapperQueryBuilder.csproj /p:ContinuousIntegrationBuild=true
+
+REM TODO: Generate symbols into its own symbols nuget (in the new snupkg format)? We would remove PDB from main nupkg (AllowedOutputExtensionsInPackageBuildOutputFolder) and pack with options --include-symbols -p:SymbolPackageFormat=snupkg
+dotnet pack DapperQueryBuilder\DapperQueryBuilder.csproj -c release -o .\packages-local\ /p:ContinuousIntegrationBuild=true
