997627b1fc2a86209ef501a6edd27ca769380210
Lars Stegman <LarsStegman@users.noreply.github.com>
2023-03-27 01:36:02 +0200

    Create constructors that take IFilters directly (#58)

--- src/DapperQueryBuilder/Filters.cs
+++ src/DapperQueryBuilder/Filters.cs
@@ -37,18 +37,33 @@ namespace DapperQueryBuilder
         #endregion
 
         #region ctor
+
         /// <summary>
         /// Create a new group of filters.
         /// </summary>
-        public Filters(FiltersType type, params FormattableString[] filters)
+        public Filters(FiltersType type, IEnumerable<IFilter> filters)
         {
             Type = type;
-            foreach (var filter in filters)
-                this.Add(new Filter(filter));
+            this.AddRange(filters);
         }
 
         /// <summary>
         /// Create a new group of filters which are combined with AND operator.
+        /// </summary>
+        public Filters(IEnumerable<IFilter> filters): this(FiltersType.AND, filters)
+        {
+        }
+
+        /// <summary>
+        /// Create a new group of filters from formattable strings
+        /// </summary>
+        public Filters(FiltersType type, params FormattableString[] filters):
+            this(type, filters.Select(fiString => new Filter(fiString)))
+        {
+        }
+
+        /// <summary>
+        /// Create a new group of filters from formattable strings which are combined with AND operator.
         /// </summary>
         public Filters(params FormattableString[] filters) : this(FiltersType.AND, filters)
         {
